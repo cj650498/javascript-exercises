050edd1df54c1fdf4cbb6ae262fd7dc90d42a9d1
Caleb Jones <cj650498@icloud.com>
2025-04-09 10:38:43 -0400

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
+/**
+ * Assignment: Write a function that simply repeats the string a given number of times.
+ * 
+ * Algorithm:
+ * 1. Check if count is negative.
+ * 2. Create an empty string to store the result.
+ * 3. Concatenate the given string to the result `count` times using a loop.
+ * 4. Return the result.
+ */
 
+const repeatString = function(text, count) {
+
+    if (count < 0) {
+        return "ERROR";
+    }
+
+    let result = '';
+    for (let i = 0; i < count; ++i) {
+        result += text;
+    }
+    return result;
 };
 
 // Do not edit below this line
