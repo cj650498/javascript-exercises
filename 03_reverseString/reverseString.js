d1d672ef4e82ea435fe77a757e2149272aa95fff
Caleb Jones <cj650498@icloud.com>
2025-04-09 10:39:06 -0400

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
+/**
+ * Assignment: Write a function called reverseString that returns its input reversed.
+ * 
+ * Algorithm:
+ * 1. Create an empty `result` string.
+ * 2. Loop backwards through the input string.
+ * 3. On each iteration, add the current character to the result string.
+ * 4. Return the result.
+ */
 
+const reverseString = function(text) {
+    let result = '';
+    for (let i = text.length - 1; i >= 0; --i) {
+        result += text[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
